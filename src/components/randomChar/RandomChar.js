6f2e4a807de61c3fc29de46430d605e99ccd998d
Victoria Petina <kira.abramovich@gmail.com>
2022-07-21 18:03:17 +1000

    fix randomchar request

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -6,10 +6,6 @@ import './randomChar.scss';
 import mjolnir from '../../resources/img/mjolnir.png';
 
 class RandomChar extends Component {
-    constructor (props) {
-        super(props);
-        this.updateChar();
-    }
 
     state = {
         char: {},
@@ -18,6 +14,10 @@ class RandomChar extends Component {
     }
 
     marvelService = new MarvelService();
+
+    componentDidMount() {
+        this.updateChar();
+    }
 
     onCharLoaded = (char) => {
         this.setState({
