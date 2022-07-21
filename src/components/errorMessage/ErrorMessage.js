bcabf8e4a17e9ab885a5f0fc57cced9eca593b87
Victoria Petina <kira.abramovich@gmail.com>
2022-07-21 17:32:19 +1000

    fixed error svg and improved description

--- src/components/errorMessage/ErrorMessage.js
+++ src/components/errorMessage/ErrorMessage.js
@@ -1,23 +1,22 @@
 const ErrorMessage = () => {
     return (
-        <>
-            <svg viewBox="0 0 100 100" y="0" x="0" xmlns="http://www.w3.org/2000/svg" version="1.1" 
-            style={{margin:'0 auto',background:'none',display:'block'}} width="200px" height="200px">
-                <g class="ldl-scale" style={{transformOrigin:'50% 50%',transform:'rotate(0deg) scale(0.8, 0.8)'}}>
-                    <g class="ldl-ani">
-                        <g class="ldl-layer">
-                            <g class="ldl-ani" style={{transform: 'scale(0.91)', transformOrigin: '50px 50px', animation: '1.11111s linear -0.740741s infinite normal forwards running breath-78034b55-9054-45b0-b80a-6a20bd20b94f'}}>
-                                <path fill="#f4e6c8" d="M50.5 25.1c-.2-.4-.8-.4-1 0L18.2 74.3c-.1.2-.1.5.1.7l.1.2h62.8c.1 0 .4 0 .5-.3.2-.3 0-.6 0-.6L50.5 25.1z" style={{fill: rgb(130, 39, 32)}}></path>
+        <div style={{textAlign: 'center'}}>
+            <svg viewBox="0 0 100 100" y="0" x="0" xmlns="http://www.w3.org/2000/svg" style={{margin:'0 auto',background:'none',display:'block'}} width="200px" height="200px">
+                <g>
+                    <g>
+                        <g>
+                            <g>
+                                <path fill="#f4e6c8" d="M50.5 25.1c-.2-.4-.8-.4-1 0L18.2 74.3c-.1.2-.1.5.1.7l.1.2h62.8c.1 0 .4 0 .5-.3.2-.3 0-.6 0-.6L50.5 25.1z"></path>
                             </g>
                         </g>
-                        <g class="ldl-layer">
-                            <g class="ldl-ani" style={{transform: 'scale(0.91)', transformOrigin: '50px 50px', animation: '1.11111s linear -0.925926s infinite normal forwards running breath-78034b55-9054-45b0-b80a-6a20bd20b94f'}}>
-                                <path fill="#c33837" d="M57.2 20.6c-1.6-2.4-4.3-3.9-7.2-3.9-2.9 0-5.6 1.4-7.3 3.9L11.5 69.7c-1.8 2.7-2 6.1-.5 9 1.5 2.9 4.5 4.6 7.7 4.6h62.5c3.2 0 6.2-1.8 7.7-4.6 1.5-2.9 1.4-6.3-.4-9L57.2 20.6zm24.6 54.2c-.2.3-.4.3-.5.3H18.5l-.2-.1c-.2-.2-.2-.4-.1-.7l31.3-49.2c.3-.4.8-.4 1 0l31.3 49.1c0 .1.2.3 0 .6z" style={{fill: rgb(2, 22, 86)}}></path>
+                        <g>
+                            <g>
+                                <path fill="#c33837" d="M57.2 20.6c-1.6-2.4-4.3-3.9-7.2-3.9-2.9 0-5.6 1.4-7.3 3.9L11.5 69.7c-1.8 2.7-2 6.1-.5 9 1.5 2.9 4.5 4.6 7.7 4.6h62.5c3.2 0 6.2-1.8 7.7-4.6 1.5-2.9 1.4-6.3-.4-9L57.2 20.6zm24.6 54.2c-.2.3-.4.3-.5.3H18.5l-.2-.1c-.2-.2-.2-.4-.1-.7l31.3-49.2c.3-.4.8-.4 1 0l31.3 49.1c0 .1.2.3 0 .6z"></path>
                             </g>
                         </g>
-                        <g class="ldl-layer">
-                            <g class="ldl-ani" style={{transform: scale(0.91),transformOrigin: '50px 50px', animation: '1.11111s linear -1.11111s infinite normal forwards running breath-78034b55-9054-45b0-b80a-6a20bd20b94f'}}>
-                                <path d="M61.9 52L57 47.1l-7 7-7-7-4.9 4.9 7 7-7 7 4.9 4.9 7-7 7 7 4.9-4.9-7-7z" fill="#e15c64" style={{fill: rgb(171, 173, 191)}}>
+                        <g>
+                            <g>
+                                <path d="M61.9 52L57 47.1l-7 7-7-7-4.9 4.9 7 7-7 7 4.9 4.9 7-7 7 7 4.9-4.9-7-7z" fill="#e15c64">
                                 </path>
                             </g>
                         </g>
@@ -25,7 +24,7 @@ const ErrorMessage = () => {
                 </g>
             </svg>
             <span>Something went wrong</span>
-        </>
+        </div>
     )
 }
 
