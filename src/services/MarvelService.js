77f228927885b8f1e98a17567b235d4116f5014a
Victoria Petina <kira.abramovich@gmail.com>
2022-09-20 07:52:14 +0300

    add characterList loading

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,8 @@
 class MarvelService {
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
-    _apiKey = 'apikey=d89008f23b3994e675ebdf933e8a3004'
+    _apiKey = 'apikey=d89008f23b3994e675ebdf933e8a3004';
+    _baseOffset = 210;
+
     getResource = async (url) => {
         let res = await fetch(url);
 
@@ -10,8 +12,8 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = async () => {
-        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=300&${this._apiKey}`);
+    getAllCharacters = async (offset = this._baseOffset) => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
         return res.data.results.map(this._transformCharacter);
     }
 
